ef8d3dde8a4ed3eb30a7744a8a3c21a996ad0280
Nazar <nazar.himin@gmail.com>
2018-11-26 18:43:59 +0000

    learning js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const userName = prompt('Enter your name');
+const greetings = 'Hello ' + userName;
+alert(greetings);
+
+const loveQuestion = parseInt(prompt('Do you love Nazarko? Write 1 if yes, 2 if no'));
+if (loveQuestion === 1) {
+    alert('Nazarko love you too');
+} else {
+    alert('Why?');
+}
+const loveStrong = parseInt (prompt('How strong do you love Nazarko. Please write number'));
+const myLoveStrong = loveStrong + 1;
+alert('Nazarko love you stronger - ' + myLoveStrong + ' =P');
+
+const userNumberMonth = parseInt(prompt('Please enter number of your month b-day'));
+const halfUserNumberMonth = userNumberMonth / 2;
+alert('half of your number month is ' + halfUserNumberMonth);
+
+
