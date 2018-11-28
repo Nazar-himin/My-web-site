5cbeabd53a6bc63eba1b22d918395a54872e156f
Nazar <nazar.himin@gmail.com>
2018-11-28 11:37:50 +0000

    deleted quadratic-equation

--- js/index.js
+++ js/index.js
@@ -15,38 +15,3 @@ alert('Nazarko love you stronger - ' + myLoveStrong + ' =P');
 const userNumberMonth = parseInt(prompt('Please enter number of your month b-day'));
 const halfUserNumberMonth = userNumberMonth / 2;
 alert('half of your number month is ' + halfUserNumberMonth);
-
-function  solveQuadrEq(a, b, c) {
-    const d = calcDiscr(a, b, c);
-    if (d > 0) {
-        const x1 = (-b + Math.sqrt(d))/(2*a);
-        const x2 = (-b - Math.sqrt(d))/(2*a);
-        return 'x1 = ' + x1 + ' x2 = ' + x2;
-    } else if (d === 0) {
-        const x = -(b/(2*a));
-        return 'x = ' + x;
-    } else {
-        return 'No solutions';
-    }
-}
-
-
-
-
-
-function calcDiscr(a, b, c) {
-    const d = (b*b) - 4*a*c; 
-    return d;
-}
-
-const a = parseFloat(prompt('Enter A'));
-const b = parseFloat(prompt('Enter B'));
-const c = parseFloat(prompt('Enter C'));
-if (a === 0) {
-    alert('Please write a > 0');
-} else {
-    const solutions = solveQuadrEq(a, b, c);
-    alert(solutions);
-}
-
-
